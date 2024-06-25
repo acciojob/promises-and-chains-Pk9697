8c4e40bb476dab53249751b3a1a1e0f86ac938f8
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-25 12:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const inputAge=document.getElementById('age')
+const inputName=document.getElementById('name')
+const submitBtn=document.getElementById('btn')
+
+const checkAge=(age)=>{
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			if(age>=18) res()
+			rej()
+		},4000)
+	})
+}
+
+submitBtn.addEventListener('click',(e)=>{
+	e.preventDefault()
+	if(!inputAge.value.trim() || !inputName.value.trim()) return
+	checkAge(inputAge.value)
+		.then(()=>alert(`Welcome, ${inputName.value}. You can vote.`))
+		.catch(()=>alert(`Oh sorry ${inputName.value}. You aren't old enough.`))
+	
+})
